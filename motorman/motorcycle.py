50d8a177ed930f9804847eeb8b3ef9edbe1538d1
lizerd123 <michael.farah234@gmail.com>
2017-05-23 20:54:07 -0400

    the finished product

--- motorman/motorcycle.py
+++ motorman/motorcycle.py
@@ -1,22 +1,51 @@
 import sys, pygame, pygame.mixer
 from pygame.locals import *
+import random
 pygame.init()
 import time
 pygame.key.set_repeat(25,25)
 person = pygame.image.load("motorman.png")
 deamon=pygame.image.load("deamon.png")
 bullet=pygame.image.load("bullet.png")
+ex=pygame.image.load("caution.png")
+doom=pygame.image.load("doom.png")
 dx=670
-dy=200
+dy=100
 posx=50
-posy=240
+posy=460
 bx=800
-
-
+bh=10
+go=0
+print(bh)
+#fpx=random.randrange(0, 600)
 screen=pygame.display.set_mode((900,700))
 x=0
 back=pygame.image.load("1.png")
 while True:
+	if bh<=0:
+		print("BEAST SLAIN")
+		sys.exit()
+
+	firepiller=random.randrange(0,60)
+	if firepiller==1:
+		dy=350
+		fx=random.randrange(0,670)
+		go=1
+	if go > 0:
+		go+=1
+		if go < 15:
+			screen.blit(ex,(fx,400))
+		if 15<go<25:
+			screen.blit(doom,(fx,400))
+			if posx<fx<posx+150:
+				print("YOU DIED")
+				sys.exit()
+		if go >25:
+			go=0
+			dy=100
+
+
+
 	
 	bx+=100
 	screen.blit(bullet,(bx,400))
@@ -33,8 +62,12 @@ while True:
 				posx=posx-20
 			if event.key==K_RIGHT:
 				posx=posx+20
-			if event.key==K_SPACE:
+			if event.key==K_SPACE and bx>700:
 				bx=posx
+	if dx<bx<dx+100 and dy<400<dy+100:
+		bh-=1
+
+		
 				
 
 
